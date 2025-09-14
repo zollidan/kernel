e4d9bec65bedc537d0e08447c266617719d17967
Stepan <zollidan@yandex.ru>
2025-09-14 15:24:11 +0300

    os name changed

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/rokitos.kernel isodir/boot/rokitos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "rokitos" {
+	multiboot /boot/rokitos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o rokitos.iso isodir
